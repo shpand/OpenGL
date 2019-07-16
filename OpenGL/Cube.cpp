fdb65c16b7ae6b31b792beae20e48267ef7fd4e1
Aliaksandr Shchamialiou <aliaksandrsh@playtika.com>
2019-07-16 18:19:17 +0300

    add calculation of points and indicies of cube

--- OpenGL/Cube.cpp
+++ OpenGL/Cube.cpp
@@ -2,9 +2,9 @@
 #include <GL/glew.h>
 
 
-Cube::Cube(float centerX, float centerY, float centerZ, float width, float height, float depth)
+Cube::Cube(const float centerX, const float centerY, const float centerZ, const float width, const float height, const float depth)
 {
-    
+    _points = Cube::CalculatePoints(centerX, centerY, centerZ, width, height, depth);
 }
 
 
@@ -17,7 +17,51 @@ std::vector<GLfloat> Cube::GetPoints()
     return _points;
 }
 
-std::vector<GLfloat> Cube::GetIndicies()
+std::vector<int> Cube::GetIndicies()
 {
     return _indicies;
+}
+
+std::vector<GLfloat> Cube::CalculatePoints(const float centerX, const float centerY, const float centerZ, const float width, const float height, const float depth)
+{
+    std::vector<GLfloat> points;
+
+    const auto radiusX = width / 2;
+    const auto radiusY = height / 2;
+    const auto radiusZ = depth / 2;
+
+    //point 1
+    points.push_back(centerX - radiusX);
+    points.push_back(centerY + radiusY);
+    points.push_back(centerZ - radiusZ);
+    //point 2
+    points.push_back(centerX - radiusX);
+    points.push_back(centerY - radiusY);
+    points.push_back(centerZ - radiusZ);
+    //point 3
+    points.push_back(centerX + radiusX);
+    points.push_back(centerY - radiusY);
+    points.push_back(centerZ - radiusZ);
+    //point 4
+    points.push_back(centerX + radiusX);
+    points.push_back(centerY + radiusY);
+    points.push_back(centerZ - radiusZ);
+    //point 5
+    points.push_back(centerX + radiusX);
+    points.push_back(centerY + radiusY);
+    points.push_back(centerZ + radiusZ);
+    //point 6
+    points.push_back(centerX + radiusX);
+    points.push_back(centerY - radiusY);
+    points.push_back(centerZ + radiusZ);
+    //point 7
+    points.push_back(centerX - radiusX);
+    points.push_back(centerY - radiusY);
+    points.push_back(centerZ + radiusZ);
+    //point 8
+    points.push_back(centerX - radiusX);
+    points.push_back(centerY + radiusY);
+    points.push_back(centerZ + radiusZ);
+
+    return points;
 }
--- OpenGL/Cube.h
+++ OpenGL/Cube.h
@@ -10,10 +10,26 @@ public:
     ~Cube();
 
     std::vector<GLfloat> GetPoints();
-    std::vector<GLfloat> GetIndicies();
+    std::vector<int> GetIndicies();
 
 private:
+    static std::vector<GLfloat> CalculatePoints(float centerX, float centerY, float centerZ, float width, float height, float depth);
+
     std::vector<GLfloat> _points;
-    std::vector<GLfloat> _indicies;
+    std::vector<int> _indicies
+    {
+        0,1,2,
+        2,3,0,
+        2,3,4,
+        4,5,2,
+        0,3,7,
+        7,4,3,
+        4,7,5,
+        5,6,7,
+        7,0,6,
+        6,1,0,
+        1,6,2,
+        2,5,6
+    };
 };
 
